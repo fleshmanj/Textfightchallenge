e8680a1dd9ecdca329980c6ea4f0a8b604b3c1c3
fleshmanj <fleshmanjoshuac@hotmail.com>
2022-03-27 12:37:56 -0500

    stuff

---
+++ client.py
@@ -0,0 +1,82 @@
+# Project/server/hole_punching/rendez_vous.py
+
+"""
+    UDP client.
+        1) Run server.py script (UDP server)
+        2) Run this script
+        2) Run this script on another computer
+        4) You should see server response message on both clients
+        5) You should see client talking to each other
+"""
+
+import socket
+import pickle
+from threading import Timer
+from typing import Tuple, List
+
+SERVER_ADRRESS = "dev.joshuasw.com"
+
+
+def close_socket(mutableBool):
+    del mutableBool[0]
+
+
+def spam_clients(sock: socket.socket, other_clients: List[Tuple[str, int]]):
+    # Only other clients should arrive here
+    for client in other_clients:
+        for x in range(10):
+            # Spam them
+            msg = "{0} send to {2}, try={1}".format(sock.getsockname(), x, client)
+            sock.sendto(msg.encode(), client)
+
+
+if __name__ == '__main__':
+    isRunning = [True]  # Mutable trick / hack ...
+    other_clients = []
+    message = 'UnrealHost dispo for a game !'.encode()
+    timer = Timer(1000.0, close_socket, kwargs=dict(mutableBool=isRunning))
+
+    server_address = (SERVER_ADRRESS, 5000)  # LAN (same computer)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        # Bind to any IP with dynamic port
+        # Works with or without it (default to localhost and dynamic IP)
+        # sock.bind(('', 0))
+
+        # To avoid waiting forever
+        sock.settimeout(3)
+
+        print("Server addr = {0}:{1}".format(server_address[0], server_address[1]))
+        print('---------------\n')
+
+        # Send a fake message to test server response
+        sent = sock.sendto(message, server_address)
+        print("Current interface: {0}:{1}".format(*sock.getsockname()))
+        print('---------------\n')
+
+        # Keep client up and running a certain time
+        timer.start()
+        try:
+            while isRunning:
+                # Check response
+                msg, addr = sock.recvfrom(4096)
+                print("Current interface: {0}:{1}".format(*sock.getsockname()))
+                print(addr, server_address)
+
+                if (addr[1] == server_address[1]):
+                    print("SERVER ::: From dedicated server !")
+                    new_client = pickle.loads(msg)
+                    print(new_client)
+                    # Keep track of all clients except yourself
+                    if (new_client != sock.getsockname() and new_client not in other_clients):
+                        other_clients.append(new_client)
+                        print(other_clients)
+                    # Spam them all
+                    spam_clients(sock, other_clients)
+                else:
+                    print("CLIENT ::: A client made contact, praise the sun !")
+                    print(msg.decode(), addr)
+                print('---------------\n')
+        except socket.error as err:
+            if (err == socket.timeout):
+                timer.cancel()
--- main.py
+++ main.py
@@ -1,9 +1,99 @@
-from flask import Flask
+import random
+
+from flask import Flask, request, render_template, Response
+
 app = Flask(__name__)
 
-@app.route("/hello")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+games = []
+max_players = 2
+
+@app.route("/", methods=['GET'])
+def landing():
+        return f"<h1 style='color:blue'>Hello there!</h1>"
+
+# @app.route("/<test>", methods=['GET', 'POST'])
+# def hello(test=None):
+#     if request.method =='POST':
+#         return f"<h1 style='color:blue'>Hello {test}!</h1>"
+#     else:
+#         return f"<h1 style='color:blue'>Hello there!</h1>"
+
+@app.route("/<game>", methods=['GET', 'POST'])
+def game_lobby(game):
+    player_list = {}
+    for i in games:
+        print(i.id)
+        print(game)
+        if str(i.id) == str(game):
+            player_num = 1
+            for player in i.players:
+                player_list[f"player{player_num}"] = player.name
+                player_num += 1
+    return player_list
+
+@app.route("/generate", methods=['POST', 'GET'])
+def make_game():
+    name = request.form['player_name']
+    joined = False
+    if not games:
+        games.append(Game(game_id()))
+    for i in games:
+        if len(i.players) < max_players:
+            i.players.append(Player(name))
+            print(f"{name} was added to game {i.id}")
+            joined = True
+    if not joined:
+        id = game_id()
+        while id is not None:
+            if id not in games:
+                current = Game(id)
+                current.players.append(Player(name))
+                games.append(current)
+                print(f"Made a new game and {name} was added to game {current.id}")
+                id = None
+
+    return "Game was made."
+
+@app.route("/list_games", methods=['GET'])
+def list_games():
+    string = ""
+    for i in games:
+        string = string + str(i.id) + " "
+    return string
+
+@app.route("/register", methods=['GET'])
+def register():
+    return render_template('register.html')
+
+
+def game_id():
+    number = random.randint(1, 1000)
+    return number
+
+
+class Player:
+
+    def __init__(self, name):
+        self.name = name
+        self.team = ""
+
+
+class Game:
+    def __init__(self, id):
+        self.id = id
+        self.players = []
+        self.teams = []
+        pass
+
+    def add_player(self, port_id):
+        player = Player(port_id)
+        self.players.append(player)
+
+    def set_team(self, player, color):
+        player.team = color
+        if player.team not in self.teams:
+            self.teams.append(player.team)
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host="localhost", port=8080)
